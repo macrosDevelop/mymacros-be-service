96721fb7ad49389674fb15730b02f0f7d13e677d
riky97 <71893803+riky97@users.noreply.github.com>
2023-03-21 18:02:25 +0100

    other clean code

--- src/routes/TestRoutes.ts
+++ src/routes/TestRoutes.ts
@@ -3,8 +3,6 @@ import HttpStatusCodes from "@src/constants/HttpStatusCodes";
 import { IReq, IRes } from "./types/express/misc";
 
 async function getTest(_: IReq, res: IRes) {
-  const response = _.body;
-  console.log("body", response);
   return res.status(HttpStatusCodes.OK).json({ status: "SUCA" });
 }
 
